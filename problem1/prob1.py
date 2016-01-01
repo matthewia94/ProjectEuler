23e28e4131b9a7bb90a90dee518ca89c56b46a68
Matt Anderson <matthewia94@gmail.com>
2016-01-01 03:02:04 -0600

    Project Euler problem 1 solution
    The solution to project euler problem 1.  A very simple iterative
    approach.

---
+++ problem1/prob1.py
@@ -0,0 +1,14 @@
+__author__ = 'matt'
+
+
+def main():
+    sum = 0
+    for i in range(1, 1000):
+        if i % 3 == 0:
+            sum += i
+        elif i%5 == 0:
+            sum += i
+    print sum
+
+if __name__ == "__main__":
+    main()
